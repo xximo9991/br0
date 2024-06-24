cc399c6b6c8ac794935f7db0bd807570be30a3c0
xximo9991 <xximo9991@gmail.com>
2024-06-24 21:45:31 +0100

    Update br0.sh

--- br0.sh
+++ br0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # تثبيت NoMachine على Ubuntu
 sudo apt update
 sudo apt install -y wget
@@ -6,7 +7,7 @@ wget https://download.nomachine.com/download/8.11/Linux/nomachine_8.11.3_4_amd64
 sudo dpkg -i nomachine_8.11.3_4_amd64.deb
 sudo apt install -y -f
 
-# Regular Colors
+# تعريف الألوان للرسائل
 Black='\e[0;30m'        # Black
 Red='\e[0;31m'          # Red
 Green='\e[0;32m'        # Green
@@ -16,7 +17,7 @@ Purple='\e[0;35m'       # Purple
 Cyan='\e[0;36m'         # Cyan
 White='\e[0;37m'        # White
 
-# Bold
+# تعريف الألوان البارزة
 BBlack='\e[1;30m'       # Black
 BRed='\e[1;31m'         # Red
 BGreen='\e[1;32m'       # Green
@@ -26,44 +27,29 @@ BPurple='\e[1;35m'      # Purple
 BCyan='\e[1;36m'        # Cyan
 BWhite='\e[1;37m'       # White
 
+# وظيفة للحصول على معلومات الهوست والبورت
+get_host_port_info() {
+    CONTAINER_NAME=$1
+    HOST=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME)
+    PORTS=$(docker port $CONTAINER_NAME | awk '{print $3}')
+    echo "Host: $HOST"
+    echo "Port: $PORTS"
+}
 
-#######################################################
-
-
-echo -ne '\033c'
-trap RM_HT_FOLDER SIGINT SIGQUIT SIGTSTP
-echo ""
-sleep 0.1
+# عرض شاشة الترحيب
 echo -e "${Cyan}    +${Yellow}--------------------------------------------------------------------------------------------------------------------------${Cyan}+"
-sleep 0.1
 echo -e "${Yellow}     |                                                                                                                        |"
-sleep 0.1
 echo -e "     |${Green}     ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗    ${Red}██████${Black}╗${Red} ██████${Black}╗${Red}  ██████${Black}╗${Red} ██${Black}╗${Red}    ██${Black}╗${Red}███████${Black}╗${Red}███████${Black}╗${Red}██████${Black}╗  ${Yellow}    |"
-sleep 0.1
 echo -e "     |${Green}    ██╔═══██╗████╗  ██║██║     ██║████╗  ██║██╔════╝    ${Red}██${Black}╔══${Red}██${Black}╗${Red}██${Black}╔══${Red}██${Black}╗${Red}██${Black}╔═══${Red}██${Black}╗${Red}██${Black}║${Red}    ██${Black}║${Red}██${Black}╔════╝${Red}██${Black}╔════╝${Red}██${Black}╔══${Red}██${Black}╗${Red}    ${Yellow} |"
-sleep 0.1
 echo -e "     |${Green}    ██║   ██║██╔██╗ ██║██║     ██║██╔██╗ ██║█████╗      ${Red}██████${Black}╔╝${Red}██████${Black}╔╝${Red}██${Black}║   ${Red}██${Black}║${Red}██${Black}║ ${Red}█${Black}╗ ${Red}██${Black}║${Red}███████${Black}╗${Red}█████${Black}╗  ${Red}██████${Black}╔╝    ${Yellow} |"
-sleep 0.1
 echo -e "     |${BGreen}    ██║   ██║██║╚██╗██║██║     ██║██║╚██╗██║██╔══╝      ${BRed}██${Black}╔══${BRed}██${Black}╗${BRed}██${Black}╔══${BRed}██${Black}╗${BRed}██${Black}║   ${Red}██${Black}║${BRed}██${Black}║${BRed}███${Black}╗${BRed}██${Black}║╚════${BRed}██${Black}║${BRed}██${Black}╔══╝  ${BRed}██${Black}╔══${BRed}██${Black}╗    ${Yellow} |"
-sleep 0.1
 echo -e "     |${BGreen}    ╚██████╔╝██║ ╚████║███████╗██║██║ ╚████║███████╗    ${BRed}██████${Black}╔╝${BRed}██${Black}║${BRed}  ██${Black}║╚${BRed}██████${Black}╔╝╚${BRed}███${Black}╔${BRed}███${Black}╔╝${BRed}███████${Black}║${BRed}███████${Black}╗${BRed}██${Black}║  ${BRed}██${Black}║    ${Yellow} |"
-sleep 0.1
 echo -e "     |${Green}     ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝    ${Black}╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝╚══════╝╚═╝  ╚═╝ ${Yellow}    |"
-sleep 0.1
 echo -e "     |                                                                                                               ${BCyan} BETA${Yellow}    |"
-sleep 0.1
 echo -e "     |                                                                                                                        |"
-sleep 0.1
 echo -e "${Cyan}    +${Yellow}--------------------------------------------------------------------------------------------------------------------------${Cyan}+${Yellow}"
-sleep 0.1
-echo -e "                                     |${BRed} Online Browser ${BYellow}by${BGreen} Hamza Hammouch${Cyan} powerd by${BPurple} linuxserver${Yellow} |"
-sleep 0.1
+echo -e "                                     |${BRed} Online Browser ${BYellow}by${BGreen} Hamza Hammouch${Cyan} powered by${BPurple} linuxserver${Yellow} |"
 echo -e "                                     ${Cyan}+${Yellow}--------------------------------------------------------${Cyan}+"
-sleep 0.1
-
-#######################################################
-
-
 echo -e "${Yellow}     +${White}-------------------------------------------------------------------${Yellow}+"
 echo -e "${White}     | ${Yellow} ID ${White} |                   ${BPurple}   Browser Name                       ${White}   |"
 echo -e "${Yellow}     +${White}-------------------------------------------------------------------${Yellow}+"
@@ -76,17 +62,8 @@ echo -e "${Yellow}     +${White}------------------------------------------------
 echo ""
 echo -e -n "$White    ${Red} [${Cyan}!Note:${Red}]$White If your choice is Chromium type $Green 1${White} not ${Red}01$White and the same principle applies to other browsers "
 echo ""
-# إضافة وظيفة للحصول على معلومات الهوست والبورت
-get_host_port_info() {
-    CONTAINER_NAME=$1
-    HOST=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME)
-    PORTS=$(docker port $CONTAINER_NAME | awk '{print $3}')
-    echo "Host: $HOST"
-    echo "Port: $PORTS"
-}
 
-#######################################################
-
+# اختيار المتصفح والتثبيت
 echo -e -n "$White    ${Red} [${Cyan}!${Red}]$White Type the$BRed ID$White "
 read -p "of your choice : " choice
 case $choice in
@@ -106,6 +83,8 @@ case $choice in
             ghcr.io/linuxserver/chromium:latest
 
         # احصل على معلومات الهوست والبورت
+        echo "Waiting for Chromium to start..."
+        sleep 10
         get_host_port_info chromium
         ;;
     2)
@@ -124,6 +103,8 @@ case $choice in
             ghcr.io/linuxserver/firefox:latest
 
         # احصل على معلومات الهوست والبورت
+        echo "Waiting for Firefox to start..."
+        sleep 10
         get_host_port_info firefox
         ;;
     3)
@@ -142,6 +123,8 @@ case $choice in
             ghcr.io/linuxserver/opera:latest
 
         # احصل على معلومات الهوست والبورت
+        echo "Waiting for Opera to start..."
+        sleep 10
         get_host_port_info opera
         ;;
     4)
@@ -160,6 +143,8 @@ case $choice in
             ghcr.io/linuxserver/mullvad-browser:latest
 
         # احصل على معلومات الهوست والبورت
+        echo "Waiting for Mullvad Browser to start..."
+        sleep 10
         get_host_port_info mullvad-browser
         ;;
     5)
@@ -178,6 +163,8 @@ case $choice in
             ghcr.io/linuxserver/edge:latest
 
         # احصل على معلومات الهوست والبورت
+        echo "Waiting for Microsoft Edge to start..."
+        sleep 10
         get_host_port_info edge
         ;;
     *)
@@ -186,38 +173,42 @@ case $choice in
         ;;
 esac
 
-#######################################################
-
-
+# معلومات إضافية وتنظيف الشاشة
 clear
 echo ""
-sleep 0.1
 echo -e -n "$White    ${Red} [${Green} ✔ ${Red}]$White Browser installation completed successfully ( •̀ ω •́ )✧"
-sleep 0.1
 echo ""
-sleep 0.1
 echo ""
-sleep 0.1
 echo -e "    ${Red} ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀${Blue}⢀⣠⣴⣾⣿⣿⣿⣶⣄⡀⠀"
-sleep 0.1
 echo -e "    ${Red} ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀${Blue}⣀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄"
-sleep 0.1
-echo -e "    ${Red} ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀${Blue}⢀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷"
-sleep 0.1
+echo -e "    ${Red} ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀${Blue}⢀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷"
 echo -e "    ${Red} ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤${Blue}⠾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⠙⣿⣿⡿"
-sleep 0.1
 echo -e "    ${Red} ⠀⠀⠀⠀⠀⢀⣠⠶⠛⠁⠀⠀${Blue}⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣄⣠⣿⡿⠁"
-sleep 0.1
 echo -e "    ${Red} ⢀⡾⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀${Blue}⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀⠀"
-sleep 0.1
 echo -e "    ${Red} ⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⡀${Blue}⠙⢿⣿⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-sleep 0.1
 echo -e "    ${Red} ⣿⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⡿⠟⢋⣤⠶⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-sleep 0.1
 echo -e "    ${Red} ⠘⣧⡀⠀⢰⣿⣶⣿⠿⠛⣩⡴⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-sleep 0.1
 echo -e "    ${Red} ⠀⠈⠛⠦⣤⣤⣤⡤⠖⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-sleep 0.1
-echo -e "    ${White}"
-sleep 0.1
+echo -e "${White}"
+echo ""
+echo -e "Host and Port Information:"
+case $choice in
+    1)
+        echo -e "${Green}Chromium:"
+        ;;
+    2)
+        echo -e "${Green}Firefox:"
+        ;;
+    3)
+        echo -e "${Green}Opera:"
+        ;;
+    4)
+        echo -e "${Green}Mullvad Browser:"
+        ;;
+    5)
+        echo -e "${Green}Microsoft Edge:"
+        ;;
+esac
+echo ""
+get_host_port_info "$CONTAINER_NAME"
 echo ""
